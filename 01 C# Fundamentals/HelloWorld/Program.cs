4f483b8d6abd13c4ca6c172dcb4171e056840b19
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-06-09 12:51:49 -0500

    test

--- 01 C# Fundamentals/HelloWorld/Program.cs
+++ 01 C# Fundamentals/HelloWorld/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string? name = Console.ReadLine(); // Read user input from the console
+
+Console.WriteLine($"Hello, {name}!"); // Output a greeting to the user
