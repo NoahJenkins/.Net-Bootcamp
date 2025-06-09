f8b4b88a59ab0fe95403632af77bda66112f4805
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-06-08 20:37:23 -0500

    helloworldexplicit

---
+++ 01 C# Fundamentals/HelloWorldExplicit/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MyTraditionalApp; // This namespace is used to organize code and avoid naming conflicts
+
+class Program //the main class of the application, classes are blueprints for creating objects
+{
+    static void Main(string[] args) // Main method is the entry point of the application
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
